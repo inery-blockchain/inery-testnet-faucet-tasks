bcc6b1583f54517ce9e0230d9333eace3e4940a8
alteregogi <ginanjar.lillunatic@gmail.com>
2023-01-10 12:05:27 +0700

    add comment on config.ts

--- alteregogi/src/lib/config.ts
+++ alteregogi/src/lib/config.ts
@@ -3,7 +3,7 @@ import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
 const url = 'http://vmi1064243.contaboserver.net:8888';
 
 const json_rpc = new JsonRpc(url);
-const private_key = '5KLBthkvdpjQVFoRP9rLd4FnTVxXnezvsStG1otbzX4wne73KjD';
+const private_key = '5KLBthkvdpjQVFoRP9rLd4FnTVxXnezvsStG1otbzX4wne73KjD'; // this is dummy account, no need to worry
 export const actor = 'alter.serv1';
 
 export const account = 'alteregogi';
