7c3cf0bd2e414596ae0912be978cb8dde8a2454e
0xangelina <120350015+0xangelina@users.noreply.github.com>
2023-01-13 05:28:33 +0800

    Add files via upload

---
+++ angelina/api/api_create.mjs
@@ -0,0 +1,50 @@
+import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'index.js'
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+const url = process.env.NODE_URL
+
+const json_rpc = new JsonRpc(url)
+const private_key = process.env.PRIVATE_KEY;
+
+const account = process.env.INERY_ACCOUNT
+const actor = process.env.INERY_ACCOUNT
+const signature  = new JsSignatureProvider([private_key]);
+
+
+const api = new Api({
+    rpc: json_rpc,
+    signatureProvider: signature
+})
+
+
+async function create(id, user, data){
+    try{
+        
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"create",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, user, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        console.log(tx) 
+        console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+// call RPC that we created in create function
+create(10, account, "Create new Data via JSON RPC")
