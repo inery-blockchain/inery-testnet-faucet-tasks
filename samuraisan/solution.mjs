2e89e5578421cdb1138ef83052ddbcdadc34903d
Reiki <56806850+KazamiHazaki@users.noreply.github.com>
2023-01-12 08:33:09 +0700

    Update solution.mjs

--- samuraisan/solution.mjs
+++ samuraisan/solution.mjs
@@ -1,12 +1,9 @@
 import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-const url = "http://154.26.155.62:8888"
+const url = "http://vmi1056634.contaboserver.net:8888"
 
 const json_rpc = new JsonRpc(url) 
-const private_key = process.env.PRIVATE_KEY
-const actor = process.env.INERY_ACCOUNT
+const private_key = "5JhAoguLMDaaouSPrQrvv5htH1DEgaX2kN8J4TXnSSuXk73ry2S"; 
+const actor = "samuraisan.serv"
 
 const account = "samuraisan"
 const signature  = new JsSignatureProvider([private_key]); 
@@ -16,7 +13,7 @@ const api = new Api({
     signatureProvider: signature
 })
 
-async function create(id, user, data){
+async function createTx(id, user, data){
     try{
         const tx = await api.transact({
             actions:[
@@ -36,19 +33,74 @@ async function create(id, user, data){
             ]
         },{broadcast:true,sign:true})
 
-        
-        console.log("=======================================================================")
-        console.log("===================== CREATE transaction details ======================")
-        console.log("=======================================================================")
+ 
         console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("Response contract :", tx.processed.action_traces[0].console)
         console.log("\n")
     }catch(error){
         console.log(error)
     }
 }
 
-async function destroy(id){
+async function readTx(id){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"read",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+        
+        console.log(tx, "\n")
+        console.log("Response contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function updateTx(id, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"update",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+       
+        console.log(tx, "\n")
+        console.log("Response contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function destroyTx(id){
     try{
         const tx = await api.transact({
             actions:[
@@ -69,11 +121,9 @@ async function destroy(id){
         },{broadcast:true,sign:true})
 
         
-        console.log("=======================================================================")
-        console.log("===================== DESTROY transaction details =====================")
-        console.log("=======================================================================")
+        
         console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("Response contract :", tx.processed.action_traces[0].console)
         console.log("\n")
     }catch(error){
         console.log(error)
@@ -81,9 +131,11 @@ async function destroy(id){
 }
 
 
-async function main(id, user, data){
-    await create(id, user, data)
-    await destroy(id)
+async function mainFunction(id, user, data){
+    await createTx(id, user, data)
+    await readTx(id)
+    await updateTx(id, data)
+    await destroyTx(id)
 }
 
-main(1, account, "CRUD Transaction via JSON RPC")
+mainFunction(1, account, "Test Transaction using JSON RPC")
