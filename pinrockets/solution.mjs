bf8b83b8154a861d718ec17858070e1f3e57045d
root <root@inery-pinrockets.asia-southeast1-b.c.banded-syntax-373112.internal>
2023-01-13 16:39:17 +0000

    Update file

---
+++ pinrockets/solution.mjs
@@ -0,0 +1,90 @@
+import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const url = "http://35.187.233.36:8888"
+
+const json_rpc = new JsonRpc(url) 
+const private_key = process.env.PRIVATE_KEY 
+const actor = process.env.INERY_ACCOUNT
+
+const account = "pinrockets"
+const signature  = new JsSignatureProvider([private_key]); 
+
+const api = new Api({
+    rpc: json_rpc,
+    signatureProvider: signature
+})
+
+async function createRecord(id, user, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"create",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, user, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        console.log("=======================================================================")
+        console.log("===================== CREATE transaction details ======================")
+        console.log("=======================================================================")
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function destroyRecord(id){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"destroy",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        console.log("=======================================================================")
+        console.log("===================== DESTROY transaction details =====================")
+        console.log("=======================================================================")
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+async function executeCRUD(id, user, data){
+    await createRecord(id, user, data)
+    await destroyRecord(id)
+}
+
+executeCRUD(1, account, "CRUD Transaction example via JSON RPC")
+
