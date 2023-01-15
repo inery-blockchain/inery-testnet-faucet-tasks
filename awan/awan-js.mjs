3f5cf5053de97ae5ea70f4c41d1d1d41750181f0
root <nodemual>
2023-01-15 03:19:40 +0000

    readme.md

---
+++ awan/awan-js.mjs
@@ -0,0 +1,110 @@
+import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+const url = process.env.NODE_URL
+
+const json_rpc = new JsonRpc(url) 
+const private_key = process.env.PRIVATE_KEY
+const actor = process.env.INERY_ACCOUNT
+
+const account = process.env.INERY_ACCOUNT
+const signature  = new JsSignatureProvider([private_key]); 
+
+const api = new Api({
+    rpc: json_rpc,
+    signatureProvider: signature
+})
+
+async function create(id, user, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"create",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, user, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function read(id){
+    try{
+        
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"read",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        console.log(tx)
+        console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function update(id, user, data){
+    try{
+        
+        const tx = await api.transact({
+            actions:[
+                {
+                  account,
+                  name:"update",
+                  authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, user, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        console.log(tx)
+        console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function awanPush(id, user, data){
+    await create(id, user, data)
+    await read(id)
+    await update(id, user, data)
+}
+
+awanPush(1, account, "Tx JSON RPC")
