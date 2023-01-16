df0db86f77df0f98758094959141ddb65ea47167
Dwentz™ <108969749+Whalealert@users.noreply.github.com>
2023-01-16 16:45:14 +0700

    Add files via upload

--- Nodex.inery/contract/create.mjs
+++ Nodex.inery/contract/create.mjs
@@ -1,21 +1,13 @@
 import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 const account = process.env.USER_ACC_NAME
-const actor = account
-const id = process.env.DATA_ID
-const create_data = process.env.CREATE_CRUD_DATA
-
+const actor = "nodex.inery"
 const json_rpc = new JsonRpc(process.env.INERY_NODE_RPC)
 const signature  = new JsSignatureProvider([process.env.INERY_PRIV_KEY]);
 
-const api = new Api({
-    rpc: json_rpc,
-    signatureProvider: signature
-})
-
-async function createCrud(id, user, data){
+async function create(id, user, data){
     try{
         const tx = await api.transact({
             actions:[
@@ -34,24 +26,18 @@ async function createCrud(id, user, data){
                 }
             ]
         },{broadcast:true,sign:true})
-        console.log("\x1b[1;7;92m")
-	console.log("CREATE transaction details")
-	console.log("\x1b[0m")
-        console.log(tx.processed)
-        console.log("\x1b[1;7;92m")
-	console.log("RPC Push transaction action CREATE details")
-	console.log("\x1b[0m")
-        console.log(tx.processed.action_traces[0].act)
-        console.log("\x1b[1;7;92m")
-	console.log(tx.processed.action_traces[0].console)
-	console.log("\x1b[0m")
+        
+        
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
     }catch(error){
         console.log(error)
-        console.log("\x1b[1;7;91m")
-	console.log("ERROR : Can't create contract ID", id)
-	console.log("DETAILS :", error.details[0].message)
-	console.log("\x1b[0m")
     }
 }
 
-createCrud(id, account, create_data)
+async function main(id, user, data){
+    await create(id, user, data)
+}
+
+main(1, account, "CRUD Transaction via JSON RPC")
--- Nodex.inery/contract/update.mjs
+++ Nodex.inery/contract/update.mjs
@@ -1,21 +1,13 @@
 import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 const account = process.env.USER_ACC_NAME
-const actor = account
-const id = process.env.DATA_ID
-const update_data = process.env.UPDATE_CRUD_DATA
-
+const actor = "nodex.inery"
 const json_rpc = new JsonRpc(process.env.INERY_NODE_RPC)
 const signature  = new JsSignatureProvider([process.env.INERY_PRIV_KEY]);
 
-const api = new Api({
-    rpc: json_rpc,
-    signatureProvider: signature
-})
-
-async function updateContract(id, data){
+async function update(id, data){
     try{
         const tx = await api.transact({
             actions:[
@@ -34,24 +26,20 @@ async function updateContract(id, data){
                 }
             ]
         },{broadcast:true,sign:true})
-	console.log("\x1b[1;7;92m")
-        console.log("U0DATE transaction details")
-        console.log("\x1b[0m")
-        console.log(tx.processed)
-        console.log("\x1b[1;7;92m")
-        console.log("RPC Push transaction action UPDATE details")
-        console.log("\x1b[0m")
-        console.log(tx.processed.action_traces[0].act)
-        console.log("\x1b[1;7;92m")
-        console.log(tx.processed.action_traces[0].console)
-        console.log("\x1b[0m")
+
+        
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
     }catch(error){
         console.log(error)
-        console.log("\x1b[1;7;91m")
-        console.log("ERROR : Can't update contract ID", id)
-        console.log("DETAILS :", error.details[0].message)
-        console.log("\x1b[0m")
     }
 }
+async function main(id, user, data){
+    await update(id, data)
+    
+}
 
-updateContract(id, update_data)
+main(1, account, "CRUD Transaction via JSON RPC")
+
+
