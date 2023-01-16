40bdd08773631dd0feebf011383a5697b3ebc035
raihanapr <98097799+raihanapr@users.noreply.github.com>
2023-01-16 10:10:57 +0700

    Update solution.mjs

--- raihanapr/solution.mjs
+++ raihanapr/solution.mjs
@@ -1,12 +1,9 @@
 import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
-
-const url = "http://45.141.122.163:8888"
+const url = "vmi1012580.contaboserver.net"
 
 const json_rpc = new JsonRpc(url) 
-const private_key = process.env.PRIVATE_KEY
-const actor = process.env.INERY_ACCOUNT
+const private_key = "5KLBth5JvzAv8NFFukUPXYp9mEQYdv1VrTNELNp5aMb6Kd3AFQYomFxGu"; 
+const actor = "raihanapr"
 
 const account = "raihanapr"
 const signature  = new JsSignatureProvider([private_key]); 
@@ -37,12 +34,59 @@ async function create(id, user, data){
         },{broadcast:true,sign:true})
 
         
-        console.log("=======================================================================")
-        console.log("===================== CREATE transaction details ======================")
-        console.log("=======================================================================")
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
+       
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function read(id){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"read",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+        
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function update(id, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"update",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+       
     }catch(error){
         console.log(error)
     }
@@ -69,12 +113,6 @@ async function destroy(id){
         },{broadcast:true,sign:true})
 
         
-        console.log("=======================================================================")
-        console.log("===================== DESTROY transaction details =====================")
-        console.log("=======================================================================")
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
     }catch(error){
         console.log(error)
     }
@@ -83,6 +121,8 @@ async function destroy(id){
 
 async function main(id, user, data){
     await create(id, user, data)
+    await read(id)
+    await update(id, data)
     await destroy(id)
 }
 
