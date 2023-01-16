df273566c1441f0bd3eee19682f8d094a509a85a
Dwentz™ <108969749+Whalealert@users.noreply.github.com>
2023-01-16 18:01:01 +0700

    Create push.mjs

---
+++ Nodex.inery/contract/push.mjs
@@ -0,0 +1,137 @@
+import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
+import dotenv from "dotenv";
+dotenv.config();
+
+const account = process.env.USER_ACC_NAME
+const json_rpc = new JsonRpc(process.env.INERY_NODE_RPC)
+const signature  = new JsSignatureProvider([process.env.INERY_PRIV_KEY]);
+
+async function create(id, user, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"create",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, user, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function read(id){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"read",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+        
+        
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function update(id, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"update",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function destroy(id){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"destroy",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        
+        
+        console.log(tx, "\n")
+        console.log("Response from contract :", tx.processed.action_traces[0].console)
+        console.log("\n")
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+async function main(id, user, data){
+    await create(id, user, data)
+    await read(id)
+    await update(id, data)
+    await destroy(id)
+}
+
+main(1, account, "CRUD Transaction via JSON")
