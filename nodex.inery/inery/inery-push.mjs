8a2eef1696cbc3eedf323290b87ac2f66f7cdbe1
Dwentz™ <108969749+Whalealert@users.noreply.github.com>
2023-01-17 20:58:11 +0700

    Update inery-push.mjs

--- nodex.inery/inery/inery-push.mjs
+++ nodex.inery/inery/inery-push.mjs
@@ -1,8 +1,12 @@
 import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
-import dotenv from "dotenv";
-dotenv.config();
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const account = process.env.USER_ACC_NAME
+const actor = account
+const id = process.env.DATA_ID
+const create_data = process.env.CREATE_CRUD_DATA
+
 const json_rpc = new JsonRpc(process.env.INERY_NODE_RPC)
 const signature  = new JsSignatureProvider([process.env.INERY_PRIV_KEY]);
 
@@ -11,7 +15,7 @@ const api = new Api({
     signatureProvider: signature
 })
 
-async function create(id, user, data){
+async function createCrud(id, user, data){
     try{
         const tx = await api.transact({
             actions:[
@@ -30,18 +34,26 @@ async function create(id, user, data){
                 }
             ]
         },{broadcast:true,sign:true})
-
-        
-        
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
+        console.log("\x1b[1;7;92m")
+	console.log("CREATE transaction details")
+	console.log("\x1b[0m")
+        console.log(tx.processed)
+        console.log("\x1b[1;7;92m")
+	console.log("RPC Push transaction action CREATE details")
+	console.log("\x1b[0m")
+        console.log(tx.processed.action_traces[0].act)
+        console.log("\x1b[1;7;92m")
+	console.log(tx.processed.action_traces[0].console)
+	console.log("\x1b[0m")
     }catch(error){
         console.log(error)
+        console.log("\x1b[1;7;91m")
+	console.log("ERROR : Can't create contract ID", id)
+	console.log("DETAILS :", error.details[0].message)
+	console.log("\x1b[0m")
     }
 }
-
-async function read(id){
+async function readContract(id){
     try{
         const tx = await api.transact({
             actions:[
@@ -60,17 +72,26 @@ async function read(id){
                 }
             ]
         },{broadcast:true,sign:true})
-        
-        
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
+	console.log("\x1b[1;7;92m")
+        console.log("READ transaction details")
+        console.log("\x1b[0m")
+        console.log(tx.processed)
+        console.log("\x1b[1;7;92m")
+        console.log("RPC Push transaction action READ details")
+        console.log("\x1b[0m")
+        console.log(tx.processed.action_traces[0].act)
+        console.log("\x1b[1;7;92m")
+        console.log(tx.processed.action_traces[0].console)
+        console.log("\x1b[0m")
     }catch(error){
         console.log(error)
+        console.log("\x1b[1;7;91m")
+        console.log("ERROR : Can't read contract ID", id)
+        console.log("DETAILS :", error.details[0].message)
+        console.log("\x1b[0m")
     }
 }
-
-async function update(id, data){
+async function updateContract(id, data){
     try{
         const tx = await api.transact({
             actions:[
@@ -89,16 +110,26 @@ async function update(id, data){
                 }
             ]
         },{broadcast:true,sign:true})
-    
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
+	console.log("\x1b[1;7;92m")
+        console.log("U0DATE transaction details")
+        console.log("\x1b[0m")
+        console.log(tx.processed)
+        console.log("\x1b[1;7;92m")
+        console.log("RPC Push transaction action UPDATE details")
+        console.log("\x1b[0m")
+        console.log(tx.processed.action_traces[0].act)
+        console.log("\x1b[1;7;92m")
+        console.log(tx.processed.action_traces[0].console)
+        console.log("\x1b[0m")
     }catch(error){
         console.log(error)
+        console.log("\x1b[1;7;91m")
+        console.log("ERROR : Can't update contract ID", id)
+        console.log("DETAILS :", error.details[0].message)
+        console.log("\x1b[0m")
     }
 }
-
-async function destroy(id){
+async function destroyContract(id){
     try{
         const tx = await api.transact({
             actions:[
@@ -116,22 +147,31 @@ async function destroy(id){
                     }
                 }
             ]
-        },{broadcast:true,sign:true})       
-        
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
+        },{broadcast:true,sign:true})
+	console.log("\x1b[1;7;92m")
+        console.log("DESTROY transaction details")
+        console.log("\x1b[0m")
+        console.log(tx.processed)
+        console.log("\x1b[1;7;92m")
+        console.log("RPC Push transaction action DESTROY details")
+        console.log("\x1b[0m")
+        console.log(tx.processed.action_traces[0].act)
+        console.log("\x1b[1;7;92m")
+        console.log(tx.processed.action_traces[0].console)
+        console.log("\x1b[0m")
     }catch(error){
         console.log(error)
+        console.log("\x1b[1;7;91m")
+        console.log("ERROR : Can't destroy contract ID", id)
+        console.log("DETAILS :", error.details[0].message)
+        console.log("\x1b[0m")
     }
 }
-
-
 async function main(id, user, data){
-    await create(id, user, data)
-    await read(id)
-    await update(id, data)
-    await destroy(id)
+    await createCrud(id, account, create_data)
+    await readContract(id)
+    await updateContract(id, update_data)
+    await destroyContract(id)
 }
 
 main(1, account, "push push ")
