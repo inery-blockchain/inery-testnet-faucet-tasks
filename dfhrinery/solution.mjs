9a4ffd2ba238eee781c88a7cf6393a69f81413d4
daffaharyan <122814564+daffaharyan@users.noreply.github.com>
2023-01-20 03:40:49 +0700

    Update solution.mjs

--- dfhrinery/solution.mjs
+++ dfhrinery/solution.mjs
@@ -1,141 +1,87 @@
-import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs/dist/index.js'
+import { Api, JsonRpc, JsSignatureProvider } from "ineryjs";
+import dotenv from "dotenv";
+dotenv.config();
 
-const url = "http://162.222.203.246:8888";
+const url = "http:/162.222.203.246:8888";
 
 const json_rpc = new JsonRpc(url);
 const private_key = "5KFph3WUW4DRRQGnC6z1ARidG5RFJtAyCJWp3YuHpAyCHekpurr";
+
 
 const account = "daffaharyan";
 const actor = "daffaharyan";
 const signature = new JsSignatureProvider([private_key]);
 
 const api = new Api({
-    rpc: json_rpc,
-    signatureProvider: signature
-})
+  rpc: json_rpc,
+  signatureProvider: signature,
+});
 
-async function develop(id, user, data){
-    try{
-        const tx = await api.transact({
-            actions:[
-                {
-                    account,
-                    name:"develop",
-                    authorization:[
-                        {
-                            actor,
-                            permission:"active"
-                        }
-                    ],
-                    data:{
-                        id, user, data
-                    }
-                }
-            ]
-        },{broadcast:true,sign:true})
+const MakeDataTranx = async (id, user, data) => {
+  const Hashdata = { id, user, data };
+  try {
+    const tx = await api.transact(
+      {
+        actions: [
+          {
+            account,
+            name: "create",
+            authorization: [
+              {
+                actor,
+                permission: "active",
+              },
+            ],
+            data: {
+              ...Hashdata,
+            },
+          },
+        ],
+      },
+      { broadcast: true, sign: true }
+    );
 
-        
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
-    }catch(error){
-        console.log(error)
-    }
-}
+    console.log(tx, "\n");
+    console.log("\nResponse data:", tx.processed.action_traces[0].console);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-async function scan_data(id){
-    try{
-        const tx = await api.transact({
-            actions:[
-                {
-                    account,
-                    name:"scan_data",
-                    authorization:[
-                        {
-                            actor,
-                            permission:"active"
-                        }
-                    ],
-                    data:{
-                        id
-                    }
-                }
-            ]
-        },{broadcast:true,sign:true})
-        
-        
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
-    }catch(error){
-        console.log(error)
-    }
-}
+const DeleteDataTranx = async (id) => {
+  try {
+    const destroyTx = await api.transact(
+      {
+        actions: [
+          {
+            account,
+            name: "destroy",
+            authorization: [
+              {
+                actor,
+                permission: "active",
+              },
+            ],
+            data: {
+              id,
+            },
+          },
+        ],
+      },
+      { broadcast: true, sign: true }
+    );
 
-async function change_data(id, data){
-    try{
-        const tx = await api.transact({
-            actions:[
-                {
-                    account,
-                    name:"change_data",
-                    authorization:[
-                        {
-                            actor,
-                            permission:"active"
-                        }
-                    ],
-                    data:{
-                        id, data
-                    }
-                }
-            ]
-        },{broadcast:true,sign:true})
-        
-        
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
-    }catch(error){
-        console.log(error)
-    }
-}
+    console.log("Record deleted by daffaharyan\n\n");
+    console.log(destroyTx, "\n");
+    console.log("responses: \n", destroyTx.processed.action_traces[0].console);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-async function delete_data(id){
-    try{
-        const tx = await api.transact({
-            actions:[
-                {
-                    account,
-                    name:"delete_data",
-                    authorization:[
-                        {
-                            actor,
-                            permission:"active"
-                        }
-                    ],
-                    data:{
-                        id
-                    }
-                }
-            ]
-        },{broadcast:true,sign:true})
-        
+const PushTranx = async (DataId, user, data) => {
+  await MakeDataTranx(DataId, user, data);
+  await DeleteDataTranx(DataId);
+};
 
-        console.log(tx, "\n")
-        console.log("Response from contract :", tx.processed.action_traces[0].console)
-        console.log("\n")
-    }catch(error){
-        console.log(error)
-    }
-}
-
-
-async function head(id, user, data){
-    await develop(id, user, data)
-    await scan_data(id)
-    await change_data(id, data)
-    await delete_data(id)
-}
-
-head(1, account, "CRUD Transaction via JSON RPC")
+PushTranx(1020, account, "test push transaction by daffaharyan");
