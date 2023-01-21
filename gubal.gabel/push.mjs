e11b02769767c899ad4fb9a69dd312511b5089d5
gubal-gabel <gubal-gabel@github.com>
2023-01-21 08:12:44 +0000

    Task 4 - gubal.gabel

---
+++ gubal.gabel/push.mjs
@@ -0,0 +1,36 @@
+import { Api, JsonRpc, RpcError, JsSignatureProvider } from 'ineryjs'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const url = "http://sys.blockchain-servers.world:8888"
+const account = process.env.ACCOUNT
+const priv_key = process.env.PRIV_KEY
+
+const json_rpc = new JsonRpc(url)
+const signature = new JsSignatureProvider([priv_key]);
+
+const api = new Api({
+  rpc: json_rpc,
+  signatureProvider: signature
+})
+
+console.log( await api.transact({
+  actions:[
+    {
+      account: account,
+      name: "create",
+      authorization: [
+	{
+	  actor: account,
+	  permission: "active"
+	}
+      ],
+      data:{
+	id: 29172,
+	user: account,
+	data: "Create record"
+      }
+    }
+  ]
+},{broadcast:true,sign:true}))
+
