6790df532c5e9559683467e6e7482a3797e975fb
yantodotid <35837931+yantodotid@users.noreply.github.com>
2023-01-21 08:50:29 +0700

    Create transaction.js

---
+++ heic/transaction.js
@@ -0,0 +1,114 @@
+import { Api, JsonRpc, JsSignatureProvider } from "ineryjs";
+import dotenv from "dotenv";
+dotenv.config();
+
+const node_url = "http://node_url";
+
+const JsonRPC = new JsonRpc(node_url);
+const PrivateKey = process.env.PRIVATE_KEY;
+
+const main_acc = "heic";
+const actor = "heic";
+const signature = new JsSignatureProvider([PrivateKey]);
+
+const api = new Api({
+  rpc: JsonRPC,
+  signatureProvider: signature,
+});
+
+const CreateRecordTx = async (id, user, data) => {
+  const Hashdata = { id, user, data };
+  try {
+    const txresult = await api.transact(
+      {
+        actions: [
+          {
+            main_acc,
+            name: "create",
+            authorization: [
+              {
+                actor,
+                permission: "active",
+              },
+            ],
+            data: {
+              ...Hashdata,
+            },
+          },
+        ],
+      },
+      { broadcast: true, sign: true }
+    );
+
+    console.log(tx);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const ReadRecordTx = async (id) => {
+  try {
+    const readresult = await api.transact(
+      {
+        actions: [
+          {
+            main_acc,
+            name: "read",
+            authorization: [
+              {
+                actor,
+                permission: "active",
+              },
+            ],
+            data: {
+              id,
+            },
+          },
+        ],
+      },
+      { broadcast: true, sign: true }
+    );
+    console.log(readId);
+    console.log("This is Read Record\n\n");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const DestroyRecordTx = async (id) => {
+  try {
+    const destroyresult = await api.transact(
+      {
+        actions: [
+          {
+            main_acc,
+            name: "destroy",
+            authorization: [
+              {
+                actor,
+                permission: "active",
+              },
+            ],
+            data: {
+              id,
+            },
+          },
+        ],
+      },
+      { broadcast: true, sign: true }
+    );
+
+    console.log("Destroy Record Result\n\n");
+    console.log(destroyTx);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const pushtx = async (DataId, user, data) => {
+  await CreateRecordTx(DataId, user, data);
+  await ReadRecordTx(DataId);
+  await DestroyRecordTx(DataId);
+};
+
+pushtx(5, main_acc, "This is testing to try RPC Push Transaction");
