e4de541d19967607afd18071ee516ee5dbbacadb
Dandi Cahyaman <95843080+Dandidcee@users.noreply.github.com>
2023-01-26 13:47:35 +0700

    Inery Task 4 : dandidce

---
+++ dandidce_task4/dandidce_task.py
@@ -0,0 +1,51 @@
+import json
+import os
+import requests
+
+url = "http://node_url"
+private_key = os.environ.get("PRIVATE_KEY")
+account = "heic"
+actor = "heic"
+
+def perform_transaction(name, data):
+    payload = {
+        "actions": [
+            {
+                "account": account,
+                "name": name,
+                "authorization": [
+                    {
+                        "actor": actor,
+                        "permission": "active"
+                    }
+                ],
+                "data": data
+            }
+        ]
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    response = requests.post(url, data=json.dumps(payload), headers=headers)
+    return json.loads(response.text)
+
+def create_transaction(id, user, data):
+    data = {"id": id, "user": user, "data": data}
+    return perform_transaction("create", data)
+
+def read_transaction(id):
+    data = {"id": id}
+    return perform_transaction("read", data)
+
+def destroy_transaction(id):
+    data = {"id": id}
+    return perform_transaction("destroy", data)
+
+def push_transaction(data_id, user, data):
+    create_transaction(data_id, user, data)
+    read_transaction(data_id)
+    destroy_transaction(data_id)
+
+push_transaction(1, account, "This is RPC Push Transaction using Python")
