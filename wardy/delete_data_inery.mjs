edf2aaa302c32ced5d678b342abc18e852f5e37d
warlovna <121307721+warlovna@users.noreply.github.com>
2023-01-28 01:57:29 +0700

    Update delete_data_inery.mjs

--- wardy/delete_data_inery.mjs
+++ wardy/delete_data_inery.mjs
@@ -1,31 +1,23 @@
 import { Api, JsonRpc, JsSignatureProvider } from '../dist/index.js'
 import * as dotenv from 'dotenv' 
+
 dotenv.config()
 
 const url = process.env.NODE_URL
-
 const json_rpc = new JsonRpc(url)
 const private_key = process.env.PRIVATE_KEY;
-
 const account = process.env.INERY_ACCOUNT
 const actor = process.env.INERY_ACCOUNT
 const signature  = new JsSignatureProvider([private_key]);
+const api = new Api({ rpc: jsonRpc, signatureProvider });
 
-// calling API
-const api = new Api({
-    rpc: json_rpc,
-    signatureProvider: signature
-})
-
-// A Function to delete data in our Valued Smart Contract, and call "dbdestroy" function on our Smart contract
 async function deleting(id){
     try{
-        // create new transaction and sign it
-        const tx = await api.transact({
+      const tx = await api.transact({
             actions:[
                 {
                   account,
-                  name:"dbdestroy", // check your name function at contact for delete
+                  name:"dbdestroy",
                   authorization:[
                         {
                             actor:"wardy"
@@ -46,5 +38,4 @@ async function deleting(id){
     }
 }
 
-// call RPC that we delete in deleting function
 deleting(10,  "Deleting Data via JSON RPC")
