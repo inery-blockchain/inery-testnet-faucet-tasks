e428797d7647bad1934a54fdf609e10c57eeb840
Dicki Prasetya <dickiprasetya176@gmail.com>
2023-02-07 11:13:28 +0700

    feat: update

---
+++ emblem1/src/main/Main.jsx
@@ -0,0 +1,45 @@
+import React, { Fragment } from "react";
+
+function Main() {
+  return (
+    <Fragment>
+      <div className="container-custom">
+        <div className="box-main">
+          <div className="btn-group">
+            <button className="btn btn-primary">Create</button>
+            <button className="btn btn-secondary">Read</button>
+            <button className="btn btn-warning">Update</button>
+            <button className="btn btn-warning">Delete</button>
+          </div>
+          <div className="body-content">
+            <form action="">
+              <div className="form-group mb-2">
+                <label htmlFor="id">ID</label>
+                <input
+                  type="text"
+                  placeholder="0000"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group mb-2">
+                <label htmlFor="id">Message</label>
+                <textarea
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                  className="form-control"
+                ></textarea>
+              </div>
+              <div className="form-group mt-2">
+                <button className="btn btn-success w-100">Action</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default Main;
