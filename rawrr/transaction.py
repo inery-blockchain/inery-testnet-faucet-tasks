deaa297f9963f0ba6956b3dd716937f84e8216c5
RowNode <rosalindgraham494@gmail.com>
2023-02-13 19:12:03 +0000

    Update file

---
+++ rawrr/transaction.py
@@ -0,0 +1,53 @@
+import requests
+import os
+
+url = "http://node_url:8888"
+private_key = os.getenv("PRIVATE_KEY")
+account = "rawrr"
+actor = "rawrr"
+
+def perform_transaction(name, data):
+    global url, private_key, account, actor
+
+    payload = {
+        "actions": [
+            {
+                "account": account,
+                "name": name,
+                "authorization": [
+                    {
+                        "actor": actor,
+                        "permission": "active"
+                    }
+                ],
+                "data": data
+            }
+        ]
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    return response.json()
+
+def create_transaction(id, user, data):
+    data = {"id": id, "user": user, "data": data}
+    return perform_transaction("create", data)
+
+def read_transaction(id):
+    data = {"id": id}
+    return perform_transaction("read", data)
+
+def destroy_transaction(id):
+    data = {"id": id}
+    return perform_transaction("destroy", data)
+
+def push_transaction(data_id, user, data):
+    create_transaction(data_id, user, data)
+    read_transaction(data_id)
+    destroy_transaction(data_id)
+
+push_transaction(1, account, "This is RPC Push Transaction using Python")
