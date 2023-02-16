745cc18b755011efe36db9b962788c275a2cd65e
blocxerz <98099317+hydiracrypto@users.noreply.github.com>
2023-02-16 22:10:19 +0700

    Add files via upload

---
+++ DAPP/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+export default App;
