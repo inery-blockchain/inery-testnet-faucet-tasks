8f2bfc98a6f400d681f6b65f0aec3d26d4928b6c
blocxerz <98099317+hydiracrypto@users.noreply.github.com>
2023-02-16 22:57:25 +0700

    Add files via upload

--- hydiracrypto/VALUE_CONTRACTS/read.mjs
+++ hydiracrypto/VALUE_CONTRACTS/read.mjs
@@ -5,7 +5,6 @@ dotenv.config()
 const account = process.env.USER_ACC_NAME
 const actor = account
 const id = process.env.DATA_ID
-const create_data = process.env.CREATE_CRUD_DATA
 
 const json_rpc = new JsonRpc(process.env.INERY_NODE_RPC)
 const signature  = new JsSignatureProvider([process.env.INERY_PRIV_KEY]);
@@ -15,13 +14,13 @@ const api = new Api({
     signatureProvider: signature
 })
 
-async function createCrud(id, user, data){
+async function readContract(id){
     try{
         const tx = await api.transact({
             actions:[
                 {
                     account,
-                    name:"create",
+                    name:"read",
                     authorization:[
                         {
                             actor,
@@ -29,30 +28,30 @@ async function createCrud(id, user, data){
                         }
                     ],
                     data:{
-                        id, user, data
+                        id
                     }
                 }
             ]
         },{broadcast:true,sign:true})
-        console.log("\x1b[1;7;92m")
-	console.log("CREATE transaction details")
-	console.log("\x1b[0m")
+	console.log("\x1b[1;7;92m")
+        console.log("READ transaction details")
+        console.log("\x1b[0m")
         console.log(tx.processed)
         console.log("\x1b[1;7;92m")
-	console.log("RPC Push transaction action CREATE details")
-	console.log("\x1b[0m")
+        console.log("RPC Push transaction action READ details")
+        console.log("\x1b[0m")
         console.log(tx.processed.action_traces[0].act)
         console.log("\x1b[1;7;92m")
-	console.log(tx.processed.action_traces[0].console)
-	console.log("\x1b[0m")
+        console.log(tx.processed.action_traces[0].console)
+        console.log("\x1b[0m")
     }catch(error){
         console.log(error)
         console.log("\x1b[1;7;91m")
-	console.log("ERROR : Can't create contract ID", id)
-	console.log("DETAILS :", error.details[0].message)
-	console.log("\x1b[0m")
+        console.log("ERROR : Can't read contract ID", id)
+        console.log("DETAILS :", error.details[0].message)
+        console.log("\x1b[0m")
     }
 }
 
-createCrud(id, account, create_data)
+readContract(id)
 
--- hydiracrypto/VALUE_CONTRACTS/update.mjs
+++ hydiracrypto/VALUE_CONTRACTS/update.mjs
@@ -5,7 +5,7 @@ dotenv.config()
 const account = process.env.USER_ACC_NAME
 const actor = account
 const id = process.env.DATA_ID
-const create_data = process.env.CREATE_CRUD_DATA
+const update_data = process.env.UPDATE_CRUD_DATA
 
 const json_rpc = new JsonRpc(process.env.INERY_NODE_RPC)
 const signature  = new JsSignatureProvider([process.env.INERY_PRIV_KEY]);
@@ -15,13 +15,13 @@ const api = new Api({
     signatureProvider: signature
 })
 
-async function createCrud(id, user, data){
+async function updateContract(id, data){
     try{
         const tx = await api.transact({
             actions:[
                 {
                     account,
-                    name:"create",
+                    name:"update",
                     authorization:[
                         {
                             actor,
@@ -29,30 +29,30 @@ async function createCrud(id, user, data){
                         }
                     ],
                     data:{
-                        id, user, data
+                        id, data
                     }
                 }
             ]
         },{broadcast:true,sign:true})
-        console.log("\x1b[1;7;92m")
-	console.log("CREATE transaction details")
-	console.log("\x1b[0m")
+	console.log("\x1b[1;7;92m")
+        console.log("U0DATE transaction details")
+        console.log("\x1b[0m")
         console.log(tx.processed)
         console.log("\x1b[1;7;92m")
-	console.log("RPC Push transaction action CREATE details")
-	console.log("\x1b[0m")
+        console.log("RPC Push transaction action UPDATE details")
+        console.log("\x1b[0m")
         console.log(tx.processed.action_traces[0].act)
         console.log("\x1b[1;7;92m")
-	console.log(tx.processed.action_traces[0].console)
-	console.log("\x1b[0m")
+        console.log(tx.processed.action_traces[0].console)
+        console.log("\x1b[0m")
     }catch(error){
         console.log(error)
         console.log("\x1b[1;7;91m")
-	console.log("ERROR : Can't create contract ID", id)
-	console.log("DETAILS :", error.details[0].message)
-	console.log("\x1b[0m")
+        console.log("ERROR : Can't update contract ID", id)
+        console.log("DETAILS :", error.details[0].message)
+        console.log("\x1b[0m")
     }
 }
 
-createCrud(id, account, create_data)
+updateContract(id, update_data)
 
