d00a1919797edc7b25664720dd9b9f74af3774c2
Cahyadi190 <125497118+Cahyadi190@users.noreply.github.com>
2023-02-19 09:12:14 +0700

    tes

--- cahyadi/src/lib/config.ts
+++ cahyadi/src/lib/config.ts
@@ -1,10 +1,10 @@
 import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
 
-const url = 'https://tas.blockchain-servers.world';
+const url = 'https://vmi1074099.contaboserver.net';
 
 const json_rpc = new JsonRpc(url);
-const private_key = '5KHt8wv2A4Ua83HKHv7mZRDa6DHNpSsrjxiJHjK5Y7nMeqgCotc'; // this is dummy account, no need to worry
-export const actor = 'cahya.serv1';
+const private_key = '5J56Qozh9XF57YScok2GUxMshtnpDCSxKVSurrhdENn7txHAEf5'; // this is dummy account, no need to worry
+export const actor = 'cahyadi22';
 
 export const account = 'cahyadi';
 const signature = new JsSignatureProvider([private_key]);
