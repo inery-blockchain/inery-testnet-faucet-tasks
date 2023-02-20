893839ffbbfeb2a9a56e845f51e2041e084fee9b
Mysterious <107887745+Mysterious098@users.noreply.github.com>
2023-02-21 01:20:53 +0300

    Update tailwind.config.cjs

--- mysterious/tailwind.config.cjs
+++ mysterious/tailwind.config.cjs
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+// Import the required modules
+const { join } = require('path')
+
+// Define the configuration
 module.exports = {
-    content: ['./src/**/*.{html,js,svelte,ts}'],
-    theme: {
-      extend: {},
-    },
-    plugins: [require("daisyui")],
-  }
+  content: [
+    // Specify the files that contain the CSS content
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'app/**/*.{js,ts,jsx,tsx}'),
+  ],
+  theme: {
+    // Extend the default theme by adding new properties or overriding existing ones
+    extend: {},
+  },
+  plugins: [], // Add any plugins here
+}
