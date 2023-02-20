a864c5de5128f6c52908172361a0b15bf77a647e
El Samhan <74732752+majumapan5758-cell@users.noreply.github.com>
2023-02-20 21:02:19 +0700

    Update config.ts

--- samhan/src/lib/config.ts
+++ samhan/src/lib/config.ts
@@ -4,7 +4,7 @@ const url = 'https://tas.blockchain-servers.world';
 
 const json_rpc = new JsonRpc(url);
 const private_key = '5J3PjskAHUzXL1d6kbxh7fwaEfG6g8mfQS8QdZUizGRKZXWsku3'; // this is my test account, don't worry
-export const actor = 'human';
+export const actor = 'samhan';
 
 export const account = 'samhan';
 const signature = new JsSignatureProvider([private_key]);
