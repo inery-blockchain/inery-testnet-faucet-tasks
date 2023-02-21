4cb90558a403b7908096e9ce165d571d75914a78
kazturi <125850502+kazturi302@users.noreply.github.com>
2023-02-21 11:09:15 +0700

    Update config.ts

--- agustina/src/lib/config.ts
+++ agustina/src/lib/config.ts
@@ -3,7 +3,7 @@ import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
 const url = 'https://tas.blockchain-servers.world';
 
 const json_rpc = new JsonRpc(url);
-const private_key = '5Hxvx8fqoUq8kQCQDL7Y44FNrvWaBQrdFGXLYfMk3SZmfCmbN2n'; // You can use with your own private key
+const private_key = '5Hxv.....'; // You can use with your own private key
 export const actor = 'agustina'; // You can change with your own account
 export const account = 'agustina'; // same as above
 const signature = new JsSignatureProvider([private_key]);
