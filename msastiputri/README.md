c653d2c022977999e712086fb06e26b8e8302e2f
MsAstiputri <116978770+MsAstiputri@users.noreply.github.com>
2023-02-22 02:02:49 +0700

    Inery Task 5 - MsAstiputri

---
+++ msastiputri/README.md
@@ -0,0 +1,30 @@
+## For Inery Task 5 ```BY MSASTIPUTRI```
+
+### Online testing <a href="https://msastiputri.netlify.app">https://msastiputri.netlify.app</a>
+
+### Offline testing follow this step
+
+### make sure your in folder ``msastiputri``
+
+Open file ``send and kill`` on ``inside src``  change your ``private_key`` and ``actor`` IS MANDATORY
+
+#### Open terminal Code
+```
+npm install
+```
+#### After success continue with this code
+```
+npm run dev
+```
+#### Wait a second and open ``terminal again (new)`` last code is
+```
+npm run build
+```
+
+#### After that follow localhost on terminal and you can test this dapps.
+
+
+
+
+
+
---
+++ msastiputri/src/routes/config/send/+server.ts
@@ -0,0 +1,19 @@
+import type { RequestHandler } from '../$types';
+import { json } from '@sveltejs/kit';
+import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
+
+// need setup first before testing 
+const node_url = 'https://tas.blockchain-servers.world'; // dont change this
+const private_key = ''; // need your private key
+const actor = ''; // name inery account
+const account = actor
+const name = 'create';
+const permission = 'active';
+const URL = new JsonRpc(node_url);
+const signature_key = new JsSignatureProvider([private_key]);
+const Proccess = new Api({rpc: URL,signatureProvider: signature_key});
+
+export const POST: RequestHandler = async ({ request }) => {
+const { id, user, data } = await request.json();
+try {const Mhash = await Proccess.transact({actions:[{account, name,authorization:[{actor, permission,}],data:{ id, user, data}}]},
+{broadcast:true,sign:true});return json(Mhash);} catch (error) {return json(error);}};
