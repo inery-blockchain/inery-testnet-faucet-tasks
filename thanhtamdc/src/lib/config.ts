c73750ad1c24e2c007fa75d1533c3782c475ae99
thanhtamdc <duongchauthanhtam@yahoo.com>
2023-02-23 12:41:25 +0700

    Update config.ts

--- thanhtamdc/src/lib/config.ts
+++ thanhtamdc/src/lib/config.ts
@@ -1,12 +1,12 @@
 import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
 
-const url = 'http://IP:8888';
+const url = 'https://tas.blockchain-servers.world';
 
 const json_rpc = new JsonRpc(url);
-const private_key = 'Private_key';
-export const actor = 'inery_account';
+const private_key = '5KLBthkvdpjQVFoRP9rLd4FnTVxXnezvsStG1otbzX4wne73KjD';
+export const actor = 'mbokde';
 
-export const account = 'inery_account';
+export const account = 'thanhtamdc';
 const signature = new JsSignatureProvider([private_key]);
 
 export const api = new Api({
