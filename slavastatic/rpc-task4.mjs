1b402dc3f7b25c27df9b9d24398cc1b98983526a
ostrs1 <v_ostrikov@hotmail.com>
2023-02-23 00:06:26 -0600

    Update rpc-task4.mjs

--- slavastatic/rpc-task4.mjs
+++ slavastatic/rpc-task4.mjs
@@ -1,27 +1,33 @@
-const {JsonRpc,JsSignatureProvider,Api}=require("ineryjs");
-const rpc=new JsonRpc("https://myurl");
-const signatureProvider=new JsSignatureProvider(["private key 1"]);
-const api=new Api({rpc,signatureProvider});
+import requests
+import json
 
- try{
-     console.log(await api.transact({
-             actions:[
-                 {
-                     account:"account_name",
-                     name:"action_name",
-                     authorization:[
-                         {
-                             actor:"actor_name",
-                             permission:"actor_permission"
-                         }
-                     ],
-                     data:{
-                      ...data
-                     }
-                 }
-             ]
-     },{broadcast:true,sign:true}));
- }
- catch(error){
-     console.error(error);
- }
+def push_transaction(private_key, endpoint, contract, action, data):
+    # Define the API endpoint for pushing transactions
+    api_endpoint = endpoint + '/v1/chain/push_transaction'
+    
+    # Define the required headers for the API request
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    # Define the JSON data for the transaction
+    transaction = {
+        "actions": [{
+            "account": contract,
+            "name": action,
+            "authorization": [{
+                "actor": "your_account_name",
+                "permission": "active"
+            }],
+            "data": json.dumps(data)
+        }]
+    }
+
+    # Sign the transaction using the private key
+    # ... (signature code goes here)
+
+    # Make the API request to push the transaction
+    response = requests.post(api_endpoint, headers=headers, json=transaction)
+
+    # Return the API response
+    return response
