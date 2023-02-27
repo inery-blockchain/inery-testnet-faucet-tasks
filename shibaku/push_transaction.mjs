b8341c2b2afabb7c9c7d6517f9501f29330f174a
Mamatanjay <88106212+Mamatanjay@users.noreply.github.com>
2023-02-27 08:14:12 +0700

    shibaku - task 4

---
+++ shibaku/push_transaction.mjs
@@ -0,0 +1,139 @@
+import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const url = process.env.node_url
+
+const json_rpc = new JsonRpc(url)
+const private_key = process.env.pkey;
+
+const account = process.env.accname
+const actor = process.env.accname
+const signature  = new JsSignatureProvider([private_key]);
+
+//Summon API
+const api = new Api({
+    rpc: json_rpc,
+    signatureProvider: signature
+})
+
+async function create(id, user, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"create",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, user, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        console.log(tx)
+        console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function read(id){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"read",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+        
+        console.log(tx)
+        console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function update(id, data){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"update",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id, data
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        console.log(tx)
+        console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function destroy(id){
+    try{
+        const tx = await api.transact({
+            actions:[
+                {
+                    account,
+                    name:"destroy",
+                    authorization:[
+                        {
+                            actor,
+                            permission:"active"
+                        }
+                    ],
+                    data:{
+                        id
+                    }
+                }
+            ]
+        },{broadcast:true,sign:true})
+
+        
+        console.log(tx)
+        console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+async function main(id, user, data){
+    await create(id, user, data)
+    await read(id)
+    await update(id, data)
+    await destroy(id)
+}
+
+main(1, account, "CRUD Transaction via JSON RPC")
