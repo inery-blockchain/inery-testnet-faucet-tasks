038f627721b5efabf706443927e264d86a5617e4
AmirMP <122886182+AmirMP12@users.noreply.github.com>
2023-03-01 05:19:08 +0330

    Update solution.go

--- amirmp/solution.go
+++ amirmp/solution.go
@@ -12,25 +12,22 @@ type IneryValueAction struct {
 }
 
 func main() {
-    // Specify the URL of the Inery Blockchain API endpoint
-    apiEndpoint := "https://api.inery.io"
+    apiEndpoint := "https://5.144.132.50:8888"
 
-    // Create a JSON object for the action data
     actionData := IneryValueAction{Value: "Hello, Inery!"}
     data, err := json.Marshal(actionData)
     if err != nil {
         panic(err)
     }
 
-    // Create a JSON object for the transaction
     transaction := map[string]interface{}{
         "actions": []map[string]interface{}{
             {
-                "account": "inery.value",
+                "account": "amirmp",
                 "name": "set",
                 "authorization": []map[string]interface{}{
                     {
-                        "actor": "your_account_name",
+                        "actor": "amirmp",
                         "permission": "active",
                     },
                 },
@@ -45,14 +42,12 @@ func main() {
         panic(err)
     }
 
-    // Send a POST request to the Inery Blockchain API to push the transaction
     resp, err := http.Post(apiEndpoint+"/v1/chain/push_transaction", "application/json", bytes.NewBuffer(jsonBody))
     if err != nil {
         panic(err)
     }
     defer resp.Body.Close()
 
-    // Print the response from the Inery Blockchain API
     fmt.Println(resp.Status)
     fmt.Println(resp.Header)
 }
