00764b49f9ac0ff7ca0f8c5833de6b4786a9c8bb
JM-03 <nicoaji03@gmail.com>
2023-03-04 16:19:12 +0700

    github update task 5

--- gesrex121/src/lib/config.ts
+++ gesrex121/src/lib/config.ts
@@ -3,7 +3,7 @@ import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
 const url = 'https://tas.blockchain-servers.world';
 
 const json_rpc = new JsonRpc(url);
-const private_key = 'your-private-key'; // only on web
+const private_key = 'your-private-key'; // only on web test
 export const actor = 'account';
 
 export const account = 'gesrex121';
