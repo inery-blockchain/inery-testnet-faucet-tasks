269f5cc6111184dbafccd6c87a9a4a638d99b676
AmirMP <122886182+AmirMP12@users.noreply.github.com>
2023-03-04 19:41:38 +0330

    Task 5 -amirmp

---
+++ amirmp/context/context.tsx
@@ -0,0 +1,126 @@
+import { actor, pushapi } from "./config";
+import { Api } from "ineryjs";
+import React, { createContext } from "react";
+
+
+interface IPropsCreate {
+  user: string;
+  data: string;
+  id: number;
+}
+
+interface ICreateContext {
+  output: any;
+  loading: boolean;
+  ReadData: (id: any) => void;
+  pushapi: Api;
+  actor: string;
+  Create: (props: IPropsCreate) => void;
+  setStatus: (status: any) => void;
+  status: string;
+}
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const GlobalContext = createContext<ICreateContext>({
+  output: {},
+  loading: false,
+  ReadData: () => {},
+  pushapi: pushapi,
+  actor: actor,
+  Create: () => {},
+  setStatus: () => {},
+  status: "",
+});
+
+export const CreateProvider = ({ children }: Props) => {
+  const [loading, setLoading] = React.useState(false);
+  const [output, setOutput] = React.useState<any>(
+    "Inery Testnet"
+  );
+  const [status, setStatus] = React.useState<any>("");
+
+  const Create = async ({ user, data, id }: IPropsCreate) => {
+    const Hashdata = { user, data, id };
+    try {
+      setLoading(true);
+      const tx = await pushapi.transact(
+        {
+          actions: [
+            {
+              account: actor,
+              name: status,
+              authorization: [
+                {
+                  actor,
+                  permission: "active",
+                },
+              ],
+              data: {
+                user, data, id,
+              },
+            },
+          ],
+        },
+        { broadcast: true, sign: true }
+      );
+      console.log(tx);
+      setOutput(tx);
+    } catch (error) {
+      console.log(error);
+      setOutput(error);
+    }
+    setLoading(false);
+  };
+
+  const ReadData = async ({ id }: IPropsCreate) => {
+    try {
+      setLoading(true);
+      const tx = await pushapi.transact(
+        {
+          actions: [
+            {
+              account: actor,
+              name: status,
+              authorization: [
+                {
+                  actor,
+                  permission: "active",
+                },
+              ],
+              data: {
+                id,
+              },
+            },
+          ],
+        },
+        { broadcast: true, sign: true }
+      );
+      console.log(tx);
+      setOutput(tx);
+    } catch (error) {
+      console.log(error);
+      setOutput(error);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        pushapi,
+        actor,
+        Create,
+        output,
+        loading,
+        ReadData,
+        setStatus,
+        status,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
