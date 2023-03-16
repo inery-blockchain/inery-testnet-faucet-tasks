c5816d6d2334bf71dedc464fe07d42fb469371a8
bedeviler47 <118633354+bedeviler47@users.noreply.github.com>
2023-03-17 02:59:03 +0300

    Add files via upload

---
+++ shadow/node_config.js
@@ -0,0 +1,5 @@
+const inery_url = ""; // change with your inery_url
+const inery_private_key = ""; // change with your private key 
+const inery_account = ""; // change with your inery account name
+
+module.exports = { inery_url, inery_private_key, inery_account };
