7877dd77550b63d7f49fddd935497a5881233dc6
goon-sc <120354604+goon-sc@users.noreply.github.com>
2023-03-17 17:13:52 +0700

    Update gotion.mjs

--- goon.dobel/gotion.mjs
+++ goon.dobel/gotion.mjs
@@ -10,80 +10,114 @@ const signatureProvider = new JsSignatureProvider([user1PrivateKey]);
 // Calling API
 const api = new Api({ rpc: json_rpc,signatureProvider: signature})
 
-const goTransaction = async (id, user, data){
-    const goData = { id, user, data }
+
+// New transact
+
+ const goTransaksi = async (id, user, data) => {
+   cosnt goData = { id, user, data }
+ }
     try{
-        const tx = await api.transact
-        ({ actions:[{ account,name:"create",authorization:
-        [{ actor,permission:"active" }], data: {...goData}}]
-        },
+        const tx = await api.transact({
+        actions:[ {  account,  name:"create",
+        authorization:[ { actor, permission:"active"
+         } ], data:{ goData }  } ]
+        },{broadcast:true,sign:true})
         
-        {broadcast:true,sign:true})
-
-        console.log(=== TRANSACTION DETAILS ====)
-        console.log(tx, "\n") 
-        console.log(tx.processed.action_traces[0].console)
-    } catch(error){
+        console.log(tx)
+	console.log("GO TRANS transaction details")
+	console.log('/n')
+        console.log(tx.processed)
+	console.log('The Result', tx.processed)
+        console.log(tx.processed.action_traces[0].act)
+	console.log(tx.processed.action_traces[0].console)
+    }catch(error){
         console.log(error)
+        console.log('/n')
+	console.log(' ERROR E ', id)
+	console.log("DETAILS :", error.details[0].message)
+	console.log("\x1b[0m"){
     }
-} ; 
-
-const read = async (id){
-    try{
-        const tx = await api.transact
-        ({ actions:[{ account,name:"read",authorization:
-        [{ actor,permission:"active" }], data: {id}}]
-        },
-        
-        {broadcast:true,sign:true})
-
-        console.log(=== READ DETAILS ====) 
-        console.log(tx, "\n")
-        console.log(tx.processed.action_traces[0].console)
-    } catch(error){
-        console.log(error)
-    }
-};
-
-const update = async ( id, user ){
-    try{
-        const tx = await api.transact
-        ({ actions:[{ account,name:"update",authorization:
-        [{ actor,permission:"active" }], data: { id, user }}]
-        },
-        
-        {broadcast:true,sign:true})
-
-        console.log(=== UPDATE  DETAILS ====)
-        console.log(tx, "\n")
-        console.log(tx.processed.action_traces[0].console)
-    } catch(error){
-        console.log(error)
-    }
-};
-
-const destroy = async (id){
-    try{
-        const tx = await api.transact
-        ({ actions:[{ account,name:"destroy",authorization:
-        [{ actor,permission:"active" }], data: {id}}]
-        },
-        
-        {broadcast:true,sign:true})
-
-        console.log(=== DESTROY DETAILS ====) 
-        console.log(tx, "\n")
-        console.log(tx.processed.action_traces[0].console)
-    } catch(error){
-        console.log(error)
-    }
-};
-
-async function goTransaksi (id, user, data){
-    await goData(id, user, data)
-    await read(id)
-    await update(id, data)
-    await destroy(id)
 }
 
-    goTransaction ( 27000, account, " Push Transaction RPC API on  Inery Blockchain by goon.dobel " )
+const reads = async (id) 
+    try{
+        const tx = await api.transact({
+        actions:[  {  account,  name:"read",
+        authorization:[ {  actor,
+        permission:"active" } ],
+        data:{ id } }  ]
+        },{broadcast:true,sign:true})
+        
+        console.log(tx)
+	console.log(" READS transaction details")
+	console.log('/n')
+        console.log(tx.processed)
+	console.log('The Results ', tx.processed)
+        console.log(tx.processed.action_traces[0].act)
+	console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+        console.log('/n')
+	console.log(' ERROR ', id)
+	console.log("DETAILS :", error.details[0].message)
+	console.log("\x1b[0m"){
+    }
+}
+
+const updates = async ( user ,id) 
+    try{
+        const tx = await api.transact({
+        actions:[  {  account,  name:"update",
+        authorization:[ {  actor,
+        permission:"active" } ],
+        data:{user , id } }  ]
+        },{broadcast:true,sign:true})
+        
+        console.log(tx)
+	console.log(" UPDATE transaction details")
+	console.log('/n')
+        console.log(tx.processed)
+	console.log('The Results ', tx.processed)
+        console.log(tx.processed.action_traces[0].act)
+	console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+        console.log('/n')
+	console.log(' ERROR ', id)
+	console.log("DETAILS :", error.details[0].message)
+	console.log("\x1b[0m"){
+    }
+}
+
+const destroyed = async (id) 
+    try{
+        const tx = await api.transact({
+        actions:[  {  account,  name:"destroy",
+        authorization:[ {  actor,
+        permission:"active" } ],
+        data:{ id } }  ]
+        },{broadcast:true,sign:true})
+        
+        console.log(tx)
+	console.log(" DESTROYED transaction details")
+	console.log('/n')
+        console.log(tx.processed)
+	console.log('The Results ', tx.processed)
+        console.log(tx.processed.action_traces[0].act)
+	console.log(tx.processed.action_traces[0].console)
+    }catch(error){
+        console.log(error)
+        console.log('/n')
+	console.log(' ERROR ', id)
+	console.log("DETAILS :", error.details[0].message)
+	console.log("\x1b[0m"){
+    }
+}
+
+async function mainTe (id, user, data){
+    await goTransaksi (id, user, data)
+    await reads(id)
+    await updates (user, id)
+    await destroyed (id)
+    
+    mainTe ( 10, account "SAMPLE transaction via RPC JSON by goon.dobel " )
