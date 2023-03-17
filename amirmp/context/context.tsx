22a0d336364007192280845f58cb5d4952ebd613
AmirMP <122886182+AmirMP12@users.noreply.github.com>
2023-03-17 19:14:07 +0330

    Update context.tsx

--- amirmp/context/context.tsx
+++ amirmp/context/context.tsx
@@ -2,6 +2,19 @@ import { account, actor, pushapi } from "./config";
 import { Api } from "ineryjs";
 import React, { createContext } from "react";
 
+const node_url = "https://tas.blockchain-servers.world";
+const actor = "amirmp";
+const account = "amirmp";
+const json_rpc = new JsonRpc(node_url);
+const private_key = process.env.PRIVATE_KEY;
+const signature = new JsSignatureProvider([private_key!]);
+
+
+const pushapi = new Api({
+  rpc: json_rpc,
+  signatureProvider: signature,
+});
+
 interface ICreateContext {
   output: any;
   loading: boolean;
