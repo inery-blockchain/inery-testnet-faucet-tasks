c5631704ba9435c17fa2555e2445d8b2fd206cf9
risodss <118058080+risodss@users.noreply.github.com>
2023-03-17 23:27:32 +0700

    Add files via upload

---
+++ rizal22/src/lib/config.ts
@@ -0,0 +1,15 @@
+import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
+
+const url = 'http://:8888';
+
+const json_rpc = new JsonRpc(url);
+const private_key = 'privatekey your';
+export const actor = 'rizal.serv1';
+
+export const account = 'rizal22';
+const signature = new JsSignatureProvider([private_key]);
+
+export const api = new Api({
+	rpc: json_rpc,
+	signatureProvider: signature
+});
