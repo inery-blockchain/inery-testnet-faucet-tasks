3e5c42eb351d9d8fa351ace8c6da61be68ad9d98
AmirMP <122886182+AmirMP12@users.noreply.github.com>
2023-03-17 16:03:58 +0330

    Update config.ts

--- amirmp/context/config.ts
+++ amirmp/context/config.ts
@@ -1,10 +1,10 @@
 import { JsSignatureProvider, Api, JsonRpc } from "ineryjs";
 
-const actor = "amirmp";
+const node_url = "https://tas.blockchain-servers.world";
+const acc = "amirmp";
+const json_rpc = new JsonRpc(node_url);
 const private_key = process.env.PRIVATE_KEY;
 const signature = new JsSignatureProvider([private_key!]);
-const node_url = "https://tas.blockchain-servers.world";
-const json_rpc = new JsonRpc(node_url);
 
 
 const pushapi = new Api({
@@ -12,4 +12,4 @@ const pushapi = new Api({
   signatureProvider: signature,
 });
 
-export { actor, pushapi };
+export { acc, pushapi };
