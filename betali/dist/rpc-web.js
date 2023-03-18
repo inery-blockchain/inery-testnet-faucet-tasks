e89b7ee3b8562489825e15894e321a627a097588
Kang Azka <121345995+kangazka@users.noreply.github.com>
2023-03-18 16:28:54 +0700

    Task 4

---
+++ betali/dist/rpc-web.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.RpcError = exports.JsonRpc = void 0;
+var ineryjs_jsonrpc_1 = require("./ineryjs-jsonrpc");
+Object.defineProperty(exports, "JsonRpc", { enumerable: true, get: function () { return ineryjs_jsonrpc_1.JsonRpc; } });
+var ineryjs_rpcerror_1 = require("./ineryjs-rpcerror");
+Object.defineProperty(exports, "RpcError", { enumerable: true, get: function () { return ineryjs_rpcerror_1.RpcError; } });
