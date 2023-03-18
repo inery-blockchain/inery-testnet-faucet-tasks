0b5695b3bb6126013b55cee29252e3b6c0fcea50
sipenhd <121775759+sipenhd@users.noreply.github.com>
2023-03-18 17:49:21 +0700

    Update config.js

--- sipen.hd/config.js
+++ sipen.hd/config.js
@@ -5,16 +5,16 @@ dotenv.config()
 
 const url = "http://165.227.141.118:8888" // submit your IP VPS or NODE URL
 const account = "sipen.hd" // submit your account name inery 
-const private_key = process.env.PRIVATE_KEY // submit your private key 
+const private_key = "process.env.PRIVATE_KEY" // submit your private key 
 
-const actor = "sipenhd" // change your name 
+const actor = "sipen.hd" // change your name 
 const json_rpc = new JsonRpc (process.env.INERY_NODE_RPC)
 const signature  = new JsSignatureProvider ([process.env.INERY_PRIV_KEY]);
 
-// Request API 
-const Api = new Api({
+// CALLING API 
+const reqApi = new Api({
     rpc: json_rpc,
     signatureProvider: signature
 })
 
-export ( Api , account , actor  ) 
+export ( reqApi , account , actor  ) 
