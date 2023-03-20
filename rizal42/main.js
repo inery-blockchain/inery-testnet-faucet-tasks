0395ce199ae6163b8725112ffe9ec4a8bdc115fd
rizaltampan2 <127279176+rizaltampan2@users.noreply.github.com>
2023-03-20 18:17:29 +0700

    Add files via upload

---
+++ rizal42/main.js
@@ -0,0 +1,55 @@
+const { Api, JsonRpc, JsSignatureProvider } = require('ineryjs');
+const express = require('express');
+const dotenv = require('dotenv');
+const axios = require('axios')
+dotenv.config();
+
+const account ="";
+const port="your port";
+const Node_url ="http://:8888";
+const Private_key=""
+const api = new Api({
+  rpc: new JsonRpc(Node_url),
+  signatureProvider: new JsSignatureProvider([Private_key])
+});
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/', async (req, res) => {
+  const action = req.body.action;
+  const dataId = parseInt(req.body.dataId);
+  const data = req.body.data;
+  if (['create', 'read', 'update', 'destroy'].includes(action) && !isNaN(dataId)) {
+    try {
+      const result = await api.transact({
+        actions: [
+          {
+            account,
+            name: action,
+            authorization: [{ actor: account, permission: 'active' }],
+            data: { id: dataId, user: account, data },
+          },
+        ],
+      });
+      console.log(result);
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(400).json({ message: 'Invalid request' });
+  }
+});
+
+(async () => {
+  const ip = await axios.get('https://api.ipify.org')
+  const port = port || 3000
+  app.listen(port, () => {
+    console.log(`Server running on http://${ip.data}:${port}`)
+  });
+})();
