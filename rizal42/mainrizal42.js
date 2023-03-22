a34edb2c924d0b92aa71610c8de6b74e426453be
rizaltampan2 <127279176+rizaltampan2@users.noreply.github.com>
2023-03-22 14:18:03 +0700

    Update and rename main.js to mainrizal42.js

--- rizal42/mainrizal42.js
+++ rizal42/mainrizal42.js
@@ -1,40 +1,44 @@
 const { Api, JsonRpc, JsSignatureProvider } = require('ineryjs');
 const express = require('express');
 const dotenv = require('dotenv');
-const axios = require('axios')
+const axios = require('axios');
+
 dotenv.config();
 
-const account ="";
-const PORT="";
-const Node_url ="http://:8888";
-const Private_key=""
+const ACCOUNT_NAME = "";
+const PORT_NUMBER = "";
+const NODE_URL = "http://:8888";
+const PRIVATE_KEY = "";
+
 const api = new Api({
-  rpc: new JsonRpc(Node_url),
-  signatureProvider: new JsSignatureProvider([Private_key])
+  rpc: new JsonRpc(NODE_URL),
+  signatureProvider: new JsSignatureProvider([PRIVATE_KEY])
 });
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('dapp42'));
 
 app.post('/', async (req, res) => {
   const action = req.body.action;
   const dataId = parseInt(req.body.dataId);
   const data = req.body.data;
+
   if (['create', 'read', 'update', 'destroy'].includes(action) && !isNaN(dataId)) {
     try {
       const result = await api.transact({
         actions: [
           {
-            account,
+            account: ACCOUNT_NAME,
             name: action,
-            authorization: [{ actor: account, permission: 'active' }],
-            data: { id: dataId, user: account, data },
+            authorization: [{ actor: ACCOUNT_NAME, permission: 'active' }],
+            data: { id: dataId, user: ACCOUNT_NAME, data },
           },
         ],
       });
+
       console.log(result);
       res.json(result);
     } catch (error) {
@@ -47,9 +51,10 @@ app.post('/', async (req, res) => {
 });
 
 (async () => {
-  const ip = await axios.get('https://api.ipify.org')
-  const port = PORT || 3000
-  app.listen(port, () => {
-    console.log(`Server running on http://${ip.data}:${port}`)
+  const ipAddress = await axios.get('https://api.ipify.org');
+  const portNumber = PORT_NUMBER || 3000;
+
+  app.listen(portNumber, () => {
+    console.log(`Server running on http://${ipAddress.data}:${portNumber}`);
   });
 })();
