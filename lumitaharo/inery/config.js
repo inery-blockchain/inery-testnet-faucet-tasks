e19f936dcc35ca6e4fcb9f599b2e22568408c904
LumitaLumita <129273345+LumitaLumita@users.noreply.github.com>
2023-03-30 01:45:09 +0700

    lumitaharo

---
+++ lumitaharo/inery/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  inery_node: {
+    url: "http://67.205.165.29:8888",
+    private_key : "private_key",
+    account_name : "lumitaharo",
+    token_name : "LUMI" //your custom token symbol
+  }
+};
