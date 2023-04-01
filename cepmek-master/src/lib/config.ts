77947cab50975ceb786a969f3584af37efe645ab
Bonansha <54245939+agunganugrah240@users.noreply.github.com>
2023-04-01 12:12:25 +0700

    Update config.ts

--- cepmek-master/src/lib/config.ts
+++ cepmek-master/src/lib/config.ts
@@ -3,10 +3,10 @@ import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs/dist/index.js';
 const url = 'https://tas.blockchain-servers.world';
 
 const json_rpc = new JsonRpc(url);
-const private_key = '5KLBthkvdpjQVFoRP9rLd4FnTVxXnezvsStG1otbzX4wne73KjD'; // this is dummy account, no need to worry
-export const actor = 'akmalj';
+const private_key = '5J7d5JGsNMRbVUJ9e2ehCXxnyeC87GgbwAc4Gsrca3AXqYj7Yow'; // this is dummy account, no need to worry
+export const actor = 'cepmek';
 
-export const account = 'akmalj';
+export const account = 'cepmek';
 const signature = new JsSignatureProvider([private_key]);
 
 export const api = new Api({
