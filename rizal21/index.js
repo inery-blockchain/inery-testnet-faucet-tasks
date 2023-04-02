df194acc7bdd2174db38297ee9392881693ee236
rizaltjr <79238915+rizaltjr@users.noreply.github.com>
2023-04-02 15:19:45 +0700

    Update index.js

--- rizal21/index.js
+++ rizal21/index.js
@@ -1,32 +1,72 @@
-import { Api, JsonRpc, JsSignatureProvider } from 'ineryjs';
-import dotenv from 'dotenv';
-dotenv.config();
+const { Api, JsonRpc, JsSignatureProvider } = require('ineryjs')
+const dotenv = require('dotenv')
+const readline = require('readline')
 
-const ACCOUNT_NAME = ACCOUNT;
-const ACTIONS = [
-  { action: 'create', data: { id: 123, account: ACCOUNT_NAME, data: 'CREATE transaction' } },
-  { action: 'read', data: { id: 123, data: 'READ transaksion' } },
-  { action: 'update', data: { id: 123, data: 'UPDATE transaksion' } },
-  { action: 'destroy', data: { id: 123, data: 'DESTROY transaksion' } }
-];
+dotenv.config()
+const envVars = process.env
 
-const rpc = new JsonRpc(NODE_URL);
-const signatureProvider = new JsSignatureProvider([PRIVET_KEY]);
-const api = new Api({ rpc, signatureProvider });
+const rpc = new JsonRpc(envVars.NODE_URL)
+const signatureProvider = new JsSignatureProvider([envVars.PRIV_KEY])
 
-async function pushTransaction(action, data = {}) {
+const api = new Api({ rpc, signatureProvider })
+const account = envVars.ACCOUNT
+const authorization = [{ actor: account, permission: 'active' }]
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('Silakan pilih tindakan yang ingin dilakukan (create/read/update/delete): ', async (action) => {
+  if (['create', 'read', 'update', 'delete'].includes(action)) {
+    const dataId = await promptDataId()
+    const data = await promptData()
+    await pushTransaction(action, { id: dataId, user: account, data })
+  } else {
+    console.error('Tindakan yang dimasukkan tidak valid.')
+    rl.close()
+  }
+})
+
+async function promptDataId() {
+  return new Promise((resolve) => {
+    rl.question('Masukkan ID Data (hanya angka >= 1): ', (dataId) => {
+      if (!isNaN(parseInt(dataId)) && parseInt(dataId) >= 1) {
+        resolve(parseInt(dataId))
+      } else {
+        console.error('ID Data harus berupa angka dan lebih besar dari atau sama dengan 1.')
+        rl.close()
+      }
+    })
+  })
+}
+
+async function promptData() {
+  return new Promise((resolve) => {
+    rl.question('Masukkan data: ', (data) => {
+      resolve(data)
+    })
+  })
+}
+
+async function pushTransaction(actionName, data) {
   try {
     const result = await api.transact({
-      actions: [api.with(ACCOUNT_NAME).as(ACCOUNT_NAME)[action](...Object.values(data))]
+      actions: [
+        {
+          account,
+          name: actionName,
+          authorization,
+          data
+        }
+      ]
     });
-    console.log(result.processed.action_traces[0])
+
+    console.log(`Detail tindakan:\n`, result.processed.action_traces[0])
+
   } catch (error) {
-    console.error(error);
+    console.error(`Terjadi kesalahan saat menjalankan transaksi ${actionName.toUpperCase()}: ${error.details[0].message}`)
+  } finally {
+    rl.close()
   }
 }
-
-for (const { action, data } of ACTIONS) {
-  await pushTransaction(action, data);
-}
-
-const abi = await api.getAbi(ACCOUNT_NAME);
